d5f08749d8ba72a051bcf81140405fb04f4f63cf
mduy2003 <mduy2003@csu.fullerton.edu>
2024-09-30 21:21:46 -0700

    Finished

--- some_heap.c
+++ some_heap.c
@@ -2,25 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "heap.h"
+#include "some_heap.h"
 
 #define KEY_NOT_PRESENT -1
 
 heap_t *heap_create(int capacity) {
+    heap_t *heap = (heap_t *)malloc(sizeof(heap_t));
+    heap->data = (heap_node_t *)malloc(capacity * sizeof(heap_node_t));
+    heap->size = 0;
+    heap->capacity = capacity;
+    return heap;
 }
 
 void heap_free(heap_t *heap) {
+    free(heap->data);
+    free(heap);
 }
 
 unsigned int heap_size(heap_t *heap) { return heap->size; }
 
-unsigned int heap_parent(unsigned int index) {  }
+unsigned int heap_parent(unsigned int index) { 
+    return (index - 1) / 2;
+}
 
-unsigned int heap_left_child(unsigned int index) { }
+unsigned int heap_left_child(unsigned int index) {
+    return 2 * index + 1;
+ }
 
-unsigned int heap_right_child(unsigned int index) {  }
+unsigned int heap_right_child(unsigned int index) { 
+    return 2 * index + 2;
+ }
 
-unsigned int heap_level(unsigned int index) {}
+unsigned int heap_level(unsigned int index) {
+    unsigned int level = 0;
+    while (index > 0) {
+        index = heap_parent(index);
+        level++;
+    }
+    return level;
+}
 
 void heap_print(heap_t *heap) {
     for (int ix = 0; ix < heap_size(heap); ix++) {
@@ -31,12 +51,35 @@ void heap_print(heap_t *heap) {
 }
 
 void heap_swap(heap_t *heap, int index1, int index2) {
+    heap_node_t temp = heap->data[index1];
+    heap->data[index1] = heap->data[index2];
+    heap->data[index2] = temp;
 }
 
 void heap_bubble_up(heap_t *heap, int index) {
+    while (index > 0 && heap->data[index].key < heap->data[heap_parent(index)].key) {
+        heap_swap(heap, index, heap_parent(index));
+        index = heap_parent(index);
+    }
 }
 
 void heap_bubble_down(heap_t *heap, int index) {
+    int min_index = index;
+    int left = heap_left_child(index);
+    int right = heap_right_child(index);
+
+    if (left < heap_size(heap) && heap->data[left].key < heap->data[min_index].key) {
+        min_index = left;
+    }
+
+    if (right < heap_size(heap) && heap->data[right].key < heap->data[min_index].key) {
+        min_index = right;
+    }
+
+    if (min_index != index) {
+        heap_swap(heap, index, min_index);
+        heap_bubble_down(heap, min_index);
+    }
 }
 
 void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
