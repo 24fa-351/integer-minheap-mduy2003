eb5ee77430c58898dc2f033e021e221fa850a8ef
mduy2003 <mduy2003@csu.fullerton.edu>
2024-12-16 00:49:38 -0800

    Reformatted

--- some_heap.c
+++ some_heap.c
@@ -6,34 +6,31 @@
 
 #define KEY_NOT_PRESENT -1
 
-heap_t *heap_create(int capacity) {
-    heap_t *heap = (heap_t *)malloc(sizeof(heap_t));
-    heap->data = (heap_node_t *)malloc(capacity * sizeof(heap_node_t));
+heap_t* heap_create(int capacity)
+{
+    heap_t* heap = (heap_t*)malloc(sizeof(heap_t));
+    heap->data = (heap_node_t*)malloc(capacity * sizeof(heap_node_t));
     heap->size = 0;
     heap->capacity = capacity;
     return heap;
 }
 
-void heap_free(heap_t *heap) {
+void heap_free(heap_t* heap)
+{
     free(heap->data);
     free(heap);
 }
 
-unsigned int heap_size(heap_t *heap) { return heap->size; }
+unsigned int heap_size(heap_t* heap) { return heap->size; }
 
-unsigned int heap_parent(unsigned int index) { 
-    return (index - 1) / 2;
-}
+unsigned int heap_parent(unsigned int index) { return (index - 1) / 2; }
 
-unsigned int heap_left_child(unsigned int index) {
-    return 2 * index + 1;
- }
+unsigned int heap_left_child(unsigned int index) { return 2 * index + 1; }
 
-unsigned int heap_right_child(unsigned int index) { 
-    return 2 * index + 2;
- }
+unsigned int heap_right_child(unsigned int index) { return 2 * index + 2; }
 
-unsigned int heap_level(unsigned int index) {
+unsigned int heap_level(unsigned int index)
+{
     unsigned int level = 0;
     while (index > 0) {
         index = heap_parent(index);
@@ -42,37 +39,44 @@ unsigned int heap_level(unsigned int index) {
     return level;
 }
 
-void heap_print(heap_t *heap) {
+void heap_print(heap_t* heap)
+{
     for (int ix = 0; ix < heap_size(heap); ix++) {
         printf("%3d - %3d : " HEAP_KEY_FORMAT "\n", heap_level(ix), ix,
-               heap->data[ix].key);
+            heap->data[ix].key);
     }
     printf("\n");
 }
 
-void heap_swap(heap_t *heap, int index1, int index2) {
+void heap_swap(heap_t* heap, int index1, int index2)
+{
     heap_node_t temp = heap->data[index1];
     heap->data[index1] = heap->data[index2];
     heap->data[index2] = temp;
 }
 
-void heap_bubble_up(heap_t *heap, int index) {
-    while (index > 0 && heap->data[index].key < heap->data[heap_parent(index)].key) {
+void heap_bubble_up(heap_t* heap, int index)
+{
+    while (index > 0
+           && heap->data[index].key < heap->data[heap_parent(index)].key) {
         heap_swap(heap, index, heap_parent(index));
         index = heap_parent(index);
     }
 }
 
-void heap_bubble_down(heap_t *heap, int index) {
+void heap_bubble_down(heap_t* heap, int index)
+{
     int min_index = index;
     int left = heap_left_child(index);
     int right = heap_right_child(index);
 
-    if (left < heap_size(heap) && heap->data[left].key < heap->data[min_index].key) {
+    if (left < heap_size(heap)
+        && heap->data[left].key < heap->data[min_index].key) {
         min_index = left;
     }
 
-    if (right < heap_size(heap) && heap->data[right].key < heap->data[min_index].key) {
+    if (right < heap_size(heap)
+        && heap->data[right].key < heap->data[min_index].key) {
         min_index = right;
     }
 
@@ -82,7 +86,8 @@ void heap_bubble_down(heap_t *heap, int index) {
     }
 }
 
-void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
+void heap_insert(heap_t* heap, heap_key_t key, heap_value_t data)
+{
     if (heap_size(heap) == heap->capacity) {
         return;
     }
@@ -94,8 +99,8 @@ void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
     heap_bubble_up(heap, heap_size(heap) - 1);
 }
 
-
-heap_value_t heap_remove_min(heap_t *heap) {
+heap_value_t heap_remove_min(heap_t* heap)
+{
     if (heap_size(heap) == 0) {
         return NULL;
     }
